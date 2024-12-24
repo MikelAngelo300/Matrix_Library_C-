8d6ccf6338c3774d639f56a783e912a9c922097b
Ryszard Mleczko <mleczko.ryszard@gmail.com>
2024-12-24 10:13:38 +0100

    Making subclasses files

--- src/Matrix.hpp
+++ src/Matrix.hpp
@@ -11,8 +11,9 @@ public:
     // Constructor
     Matrix(int rows, int cols, T initValue = T())
         : rows(rows), cols(cols) {
-        // Dynamic memory allocation
-        m_data = new T*[rows];
+
+        m_data = new T*[rows];   // Dynamic memory allocation
+
         for (int i = 0; i < rows; ++i) {
             m_data[i] = new T[cols];
             for (int j = 0; j < cols; ++j) {
@@ -21,45 +22,39 @@ public:
         }
     }
 
-    // Destructor (frees memory)
-    ~Matrix() {
+    ~Matrix() {      // Destructor (frees memory)
         for (int i = 0; i < rows; ++i) {
             delete[] m_data[i];
         }
         delete[] m_data;
     }
 
-    int getRows() const { return rows; }
+    int getRows() const { return rows; }     // Getter for rows
 
-    // Getter for columns
-    int getCols() const { return cols; }
+    int getCols() const { return cols; }     // Getter for columns
 
-    // Getter for data
-    T** getData() const { return m_data; }
+    T** getData() const { return m_data; }   // Getter for data
 
-    // Getter for value
-    T getVal(int i, int j) const {
-        // Check if indices are within range
-        if (i >= rows || j >= cols || i < 0 || j < 0)
+    T getVal(int i, int j) const {           // Getter for value
+        if (i >= rows || j >= cols || i < 0 || j < 0) // Check if indices are within range
             std::cout << "Out of range" << std::endl;
         return m_data[i][j];  // Changed to type T
     }
 
-    // Setter for value
-    void setVal(int i, int j, T newVal) {
+    void setVal(int i, int j, T newVal) {    // Setter for value
         if (i >= rows || j >= cols || i < 0 || j < 0)
             std::cout << "Out of range" << std::endl;
         m_data[i][j] = newVal;  // Changed to type T
     }
 
-    template<typename U> // Declaration as a template
+    template<typename U>
     friend std::ostream& operator<<(std::ostream& os, const Matrix<U>& M);
 
 };
 
-// Operator << for printing the matrix
+
 template<typename U>
-std::ostream& operator<<(std::ostream& os, const Matrix<U>& M) {
+std::ostream& operator<<(std::ostream& os, const Matrix<U>& M) {  // Operator << for printing the matrix
     for (int i = 0; i < M.getRows(); ++i) {      // Loop through rows
         for (int j = 0; j < M.getCols(); ++j) {  // Loop through columns
             os << M.getData()[i][j] << " ";      // Displaying element
